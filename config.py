7dbc5dbf273e0f2df84c94c9cd1db3d322bcc2cb
sree <sri59776@gmail.com>
2021-06-16 13:53:30 +0530

    review comments

--- config.py
+++ config.py
@@ -6,7 +6,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+USERNAME = os.getenv('APP_USERNAME')
+PASSWORD = os.getenv('PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
 # Connect to the database
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyurr'
+SQLALCHEMY_DATABASE_URI = f'postgresql://{USERNAME}:{PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
